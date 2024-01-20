b729e7216c55fb86c726a49b56b83f6deb75d909
Programme0 <farjanislam2000@gmail.com>
2024-01-20 13:53:55 +0600

    add

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include<string>
+#include<stack>
 
 using namespace std;
+
+class courier{
+public:
+    int ID;
+    string sender;
+    string receiver;
+    string destination;
+
+
+
+
+
+};
 
 int main()
 {
