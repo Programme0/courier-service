c1cd168db63b6d0959a09ce9ea0ca4b3b08fea3e
Programme0 <farjanislam2000@gmail.com>
2024-01-29 12:44:26 +0600

    add

--- main.cpp
+++ main.cpp
@@ -3,22 +3,43 @@
 #include<stack>
 
 using namespace std;
+class coustomers{
+private:
+    string name;
+    string address;
+    string email;
+    int destination;
+    string receiver name;
 
-class courier{
 public:
-    int ID;
-    string sender;
-    string receiver;
-    string destination;
+    costomers(string name,string address,string email,int destination,string receiver name)
+    {
+        name=name;
+        address=address;
+        email=email;
+        destination=destination;
+        receiver name=receiver name;
+    }
 
+};
+class item{
+private:
+    string product name;
+    int quantity;
+    int weight;
 
-
-
+public:
+    item(string product name,int quantity,int weight)
+    {
+        product name=product name;
+        quantity=quantity;
+        weight=weight;
+    }
 
 };
 
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
-}
+class courier{
+public:
+
+
+};
